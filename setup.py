4cc259d75938b31dd241f8099d2719bcfe12e4ac
Adriel Vinicius <adriel1234.1234@gmail.com>
2024-10-24 14:12:22 -0400

    add files

--- setup.py
+++ setup.py
@@ -1,1 +1,20 @@
-print("hello")
+from setuptools import find_packages, setup
+
+with open("README.md", "r") as f:
+    page_description = f.read()
+
+with open("requirements.txt") as f:
+    requirements = f.read().splitlines()
+
+setup(
+    name="package_name",
+    version="0.0.1",
+    author="Adriel",
+    description="processing image python",
+    long_description=page_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/adriel1234",
+    packages=find_packages(),
+    install_requires=requirements,
+    python_requires='>=3.8',
+)
